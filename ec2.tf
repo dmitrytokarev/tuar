4b32ed394bf58184c6a70755254c410d50cec977
Dmitry Tokarev <dm.tokarev@yahoo.com>
2018-04-27 02:51:21 -0700

    ec2 with userdata

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,30 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "${var.aws_region}"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-5c97f024" // us-west-2 Ubuntu 16.04
-  instance_type = "t2.micro"
+  ami                    = "ami-ca89eeb2"                        // us-west-2 Ubuntu 16.04
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.example.id}"]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, there" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
 
   tags {
     Name = "test"
+  }
+}
+
+resource "aws_security_group" "example" {
+  name = "tf-example-sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
